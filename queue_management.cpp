8d357e6f3e84db8e880e81a1da46c3b8cc6ebe42
jamil <jamil.bce@gmail.com>
2024-05-09 22:01:34 +0500

    edited comments.

--- queue_management.cpp
+++ queue_management.cpp
@@ -1,7 +1,9 @@
 /* PRACTICE EXERCISE
 Queue Management
 
-It's time to update your Queue management system. The previous version supports only integer numbers and we need to support more types, such as strings, to store customer names in the queue.
+It's time to update your Queue management system. 
+The previous version supports only integer numbers
+ and we need to support more types, such as strings, to store customer names in the queue.
 
 Transform the given Queue class to a class template, which can work with different data types.
 
