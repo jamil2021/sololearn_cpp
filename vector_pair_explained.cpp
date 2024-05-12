569b18e32725e1db99c0208e9ff479edc6c54e71
jamil <jamil.bce@gmail.com>
2024-05-12 09:05:45 +0500

    vector, data.push_back(make_pair(x,y)); explained.

---
+++ vector_pair_explained.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <utility> // for std::pair
+
+using namespace std;
+
+int main() {
+    // Declare a vector of pairs to store x and y values
+    vector<pair<int, int>> data;
+
+    // Example values for x and y
+    int x = 42;
+    int y = 73;
+
+    // Add the pair (x, y) to the vector
+    data.push_back(make_pair(x, y));
+
+    // Accessing elements in the vector
+    cout << "First element: (" << data[0].first << ", " << data[0].second << ")" << endl;
+
+    return 0;
+}
