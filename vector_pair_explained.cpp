8aafda5db06b1841083042fa665242b1be31b8c5
jamil <jamil.bce@gmail.com>
2024-05-12 09:06:33 +0500

    added second row in the data and accessed it.

--- vector_pair_explained.cpp
+++ vector_pair_explained.cpp
@@ -18,5 +18,14 @@ int main() {
     // Accessing elements in the vector
     cout << "First element: (" << data[0].first << ", " << data[0].second << ")" << endl;
 
+    x = 50;
+    y = 100;
+
+    // Add the pair (x, y) to the vector
+    data.push_back(make_pair(x, y));
+
+    // Accessing elements in the vector
+    cout << "Second element: (" << data[1].first << ", " << data[1].second << ")" << endl;
+
     return 0;
 }
