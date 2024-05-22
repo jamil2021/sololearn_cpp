407ef6fa170eb922cda76e1fa7675c9e0218ca03
jamil <jamil.bce@gmail.com>
2024-05-22 07:16:07 +0500

    composition practice.

---
+++ composition_practice.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Engine {
+    public:
+        Engine(int p): power(p) {};
+        void start() {
+            cout <<"Engine ON ("<<power<<" horsepower)";
+        }
+    private:
+        int power;
+};
+class Car {
+    public:
+        Car(Engine x, string c, int y): e(x), color(c), year(y) {};
+        void start() {
+            cout <<"Starting"<<endl;
+            //your code goes here
+            
+        }
+    private:
+        Engine e;
+        string color;
+        int year;
+};
+
+int main() {
+    int power;
+    string color;
+    int year;
+    cin >> power >> color >> year;
+    
+    Engine e(power);
+    //your code goes here
+
+}
